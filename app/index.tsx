b2d95a1857114074ff07f70b020075b31bc219e5
Ehtisham Rehmat <a7rehmat@gmail.com>
2024-04-21 21:04:49 +0500

    initial landing page done

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,62 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
+import { useAssets } from "expo-asset";
+import { ResizeMode, Video } from "expo-av";
+import { Link } from "expo-router";
+import { defaultStyles } from "@/constants/Styles";
+import Colors from "@/constants/Colors";
 
 const Page = () => {
+  const [assets] = useAssets([require("@/assets/videos/intro.mp4")]);
+
   return (
     <View style={styles.container}>
-      <Text>this is index page, hello world</Text>
+      {assets && (
+        <Video
+          isMuted={true}
+          isLooping={true}
+          shouldPlay={true}
+          source={{ uri: assets[0].uri }}
+          style={styles.video}
+          resizeMode={ResizeMode.COVER}
+        />
+      )}
+
+      <View style={{ marginTop: 60, padding: 20 }}>
+        <Text style={styles.header}>
+          Ready to change the way you manage money?
+        </Text>
+      </View>
+
+      <View style={styles.buttons}>
+        <Link
+          href={"/signup"}
+          style={[
+            defaultStyles.pillButton,
+            { flex: 1, backgroundColor: "#fff" },
+          ]}
+          asChild
+        >
+          <TouchableOpacity>
+            <Text style={{ fontSize: 22, fontWeight: "500" }}>Sign up</Text>
+          </TouchableOpacity>
+        </Link>
+
+        <Link
+          href={"/login"}
+          asChild
+          style={[
+            defaultStyles.pillButton,
+            { flex: 1, backgroundColor: Colors.dark },
+          ]}
+        >
+          <TouchableOpacity>
+            <Text style={{ color: "white", fontSize: 22, fontWeight: "500" }}>
+              Login
+            </Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
     </View>
   );
 };
@@ -12,9 +64,24 @@ const Page = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  video: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+  },
+  header: {
+    fontSize: 32,
+    fontWeight: "900",
+    color: "white",
+    textTransform: "uppercase",
+  },
+  buttons: {
     justifyContent: "center",
+    flexDirection: "row",
+    gap: 20,
+    marginBottom: 60,
   },
 });
 export default Page;
